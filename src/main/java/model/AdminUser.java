922ae76525a9b60265d6c4c5159e26f9a1ec31a0
Nguyen Tien Son <ntson171201@gmail.com>
2025-07-22 01:46:11 +0700

    update

--- src/main/java/model/AdminUser.java
+++ src/main/java/model/AdminUser.java
@@ -5,52 +5,28 @@ import lombok.*;
 
 @Entity
 @Table(name = "AdminUser")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminUser {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(nullable = false, unique = true, length = 100)
     private String username;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(length = 100)
     private String fullName;
 
-    public AdminUser() {
-    }
+    @Column(length = 20)
+    private String role = "ADMIN";
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-    
+    private boolean active = true;
 }
